b4b24de7e2159eddde840e8d5d2113a9548c922e
rasya nazra <rasyanazraditya@gmail.com>
2024-10-08 16:37:21 +0700

    menambahkan 1 file bernama tugaswpu.js

---
+++ js/tugaswpu.js
@@ -0,0 +1,12 @@
+//function
+
+function vkubus(a,b){
+   const hitungA = a ** 3;
+   const hitungB = b ** 3;
+
+   const hitungseluruh = hitungA + hitungB;
+
+   return hitungseluruh;
+}
+
+console.log(vkubus(3,6));
